6c6520944f9fa19657c2c16a3e782b06fa4f8fa6
CODE-SAMRAT <paharisamrat@gmail.com>
2023-07-10 20:55:10 +0545

    404 pagenot found completed

--- src/pages/404/PageNotFound.jsx
+++ src/pages/404/PageNotFound.jsx
@@ -1,12 +1,19 @@
-import React from 'react'
+import React from "react";
+
 import "./style.scss";
+import ContentWrapper from "../../component/contentWrapper/ContentWrapper";
+
+
 
 const PageNotFound = () => {
-  return (
-    <div>
-      
-    </div>
-  )
-}
+    return (
+        <div className="pageNotFound">
+            <ContentWrapper>
+                <span className="bigText">404</span>
+                <span className="smallText">Page not found!</span>
+            </ContentWrapper>
+        </div>
+    );
+};
 
-export default PageNotFound
+export default PageNotFound;  
